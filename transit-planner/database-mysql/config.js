3aa7291364e16690bac27e75d2a3b5c3ac644154
Meifang Lin <meiflin123@gmail.com>
2018-09-08 13:47:51 -0700

    connect to mysql

--- transit-planner/database-mysql/config.js
+++ transit-planner/database-mysql/config.js
@@ -4,3 +4,4 @@ module.exports = {
   password: '',
   database: 'transit_planner'
 };
+
--- transit-planner/database-mysql/index.js
+++ transit-planner/database-mysql/index.js
@@ -3,6 +3,14 @@ const mysqlConfig = require('./config.js');
 
 const connection = mysql.createConnection(mysqlConfig);
 
+connection.connect((err) => {
+	if (err) {
+		console.log('problem connecting to mysql', err);
+		return;
+	}
+	console.log('connected to mysql!')
+})
+
 const getAllLines = function(callback) {
   // TODO - your code here!
 
