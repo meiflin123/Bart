21eed6e029e7477977d58ed7d49834db33be8732
Meifang Lin <meiflin123@gmail.com>
2018-09-12 22:13:21 -0700

    add getStops function to query the database for stops found along a line

--- transit-planner/database-mysql/index.js
+++ transit-planner/database-mysql/index.js
@@ -11,6 +11,7 @@ connection.connect((err) => {
 	console.log('connected to mysql!')
 })
 
+// a function to retrieve all of the lines from the `service_lines` table in the database.
 const getAllLines = function(callback) {
   connection.query('SELECT * FROM service_lines', (err, data) => {
   	if (err) {
@@ -27,6 +28,24 @@ getAllLines(function(err, data) {
 	console.log(data);
 })
 */
+
+//a function that will query the database for all of the stops found along a line, according to that line's `id`.
+const getStops = function(lineid, callback) {
+  var query = 'SELECT *  FROM stations, stops WHERE stops.line_id = ? AND stations.id = stops.station_id;'
+  connection.query(query, lineid, (err, data) => {
+    if (err) {
+      callback(err);
+      return;
+    }
+    callback(null, data)
+    console.log('database: getStops', data)
+  })
+}
+
+//test getStops
+getStops(1, function(err, data) {
+  console.log(data);
+})
 
 module.exports = {
   getAllLines
