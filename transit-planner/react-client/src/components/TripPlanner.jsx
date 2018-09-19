507ed813dcead25d273524ede221996014d8c330
Meifang Lin <meiflin123@gmail.com>
2018-09-18 17:42:46 -0700

    implement getStationList as ajax request in the tripplanner component

--- transit-planner/react-client/src/components/TripPlanner.jsx
+++ transit-planner/react-client/src/components/TripPlanner.jsx
@@ -1,89 +1,106 @@
 import React from 'react';
+import $ from 'jquery';
+import axios from 'axios';
 
 
-const TripPlanner = function() {
-  return (
-    <div className="trip-planner-view">
-      <div className="selections">
-        Start: 
-        <select>
-          <option>Hardcoded Station Name A</option>
-          <option>Hardcoded Station Name B</option>
-          <option>Hardcoded Station Name C</option>
-          <option>Hardcoded Station Name D</option>
-          <option>Hardcoded Station Name E</option>
-          <option>Hardcoded Station Name F</option>
-        </select>
+class TripPlanner extends React.Component {
+  constructor(props) {
+    super(props);
 
-        <br />
+    this.state = {
+      stationList: []
+    }
 
-        End: 
-        <select>
-          <option>Hardcoded Station Name A</option>
-          <option>Hardcoded Station Name B</option>
-          <option>Hardcoded Station Name C</option>
-          <option>Hardcoded Station Name D</option>
-          <option>Hardcoded Station Name E</option>
-          <option>Hardcoded Station Name F</option>
-        </select>
+  }
 
-        <br />
+  getStationList() {
+     axios.get('/api/stations/')
+     
+      .then((response) => {
+        var stations = response.data
+        this.setState({stationsList: stations})
+      })
+      .catch((error)=>{
+         console.log(error);
+       })
+  }
 
-        <button>Go!</button>
+
+
+  render() {
+    return (
+      <div className="trip-planner-view">
+        <div className="selections">
+          Start: 
+          <select>{this.state.stationList.map((station) => 
+            <option key={station.id}>{station.name}</option>)}
+          </select>
+
+          <br />
+
+          End: 
+          <select>{this.state.stationList.map((station) => 
+            <option key={station.id}>{station.name}</option>)}
+          </select>
+
+          <br />
+
+          <button>Go!</button>
+        </div>
+
+        <div className="directions">
+          <div className="directions-summary">
+            <p className="line-name">Station A to Station E</p>
+            <p>31 minutes (arrive at 5:51pm)</p>
+          </div>
+
+          <div className="directions-step">
+            <div className="directions-line-header">
+              <p className="line-name">Start at Station A</p>
+            </div>
+          </div>
+
+          <div className="directions-step">
+            <div className="directions-line-header">
+              <div className="line-circle" style={{backgroundColor: "#ed1d24"}}></div>
+              <p className="line-name">Red Line</p>
+              <p className="line-direction">towards Station C</p>
+            </div>
+            <ul>
+              <li> Station A </li>
+              <li> Station B </li>
+              <li> Station C </li>
+            </ul>
+          </div>
+
+          <div className="directions-step">
+            <div className="directions-line-header">
+              <p className="line-name">Change Trains</p>
+            </div>
+          </div>
+
+          <div className="directions-step">
+            <div className="directions-line-header">
+              <div className="line-circle" style={{backgroundColor: "#0099cc"}}></div>
+              <p className="line-name">Blue Line</p>
+              <p className="line-direction">towards Station F</p>
+            </div>
+            <ul>
+              <li> Station C </li>
+              <li> Station D </li>
+              <li> Station E </li>
+            </ul>
+          </div>
+
+          <div className="directions-step">
+            <div className="directions-line-header">
+              <p className="line-name">Arrive at Station E</p>
+            </div>
+          </div>
+        </div>
       </div>
-
-      <div className="directions">
-        <div className="directions-summary">
-          <p className="line-name">Station A to Station E</p>
-          <p>31 minutes (arrive at 5:51pm)</p>
-        </div>
-
-        <div className="directions-step">
-          <div className="directions-line-header">
-            <p className="line-name">Start at Station A</p>
-          </div>
-        </div>
-
-        <div className="directions-step">
-          <div className="directions-line-header">
-            <div className="line-circle" style={{backgroundColor: "#ed1d24"}}></div>
-            <p className="line-name">Red Line</p>
-            <p className="line-direction">towards Station C</p>
-          </div>
-          <ul>
-            <li> Station A </li>
-            <li> Station B </li>
-            <li> Station C </li>
-          </ul>
-        </div>
-
-        <div className="directions-step">
-          <div className="directions-line-header">
-            <p className="line-name">Change Trains</p>
-          </div>
-        </div>
-
-        <div className="directions-step">
-          <div className="directions-line-header">
-            <div className="line-circle" style={{backgroundColor: "#0099cc"}}></div>
-            <p className="line-name">Blue Line</p>
-            <p className="line-direction">towards Station F</p>
-          </div>
-          <ul>
-            <li> Station C </li>
-            <li> Station D </li>
-            <li> Station E </li>
-          </ul>
-        </div>
-
-        <div className="directions-step">
-          <div className="directions-line-header">
-            <p className="line-name">Arrive at Station E</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default TripPlanner;
